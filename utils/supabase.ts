f46076604a5ac0059f7115c0a3d3e0c11f781c72
jlevytravail <163339885+jlevytravail@users.noreply.github.com>
2025-08-08 16:02:44 +0200

    Updated supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
 // Configuration Supabase - Remplacez par vos vraies valeurs
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://yrkjdoynzcvagcqmzmgw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlya2pkb3luemN2YWdjcW16bWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NTc0ODEsImV4cCI6MjA3MDIzMzQ4MX0.Q-Jyw6EPsrKkFpepFaUI8Czt3DP_kbrVwSVLxRSld5U';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
